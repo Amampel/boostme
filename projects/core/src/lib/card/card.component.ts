c61f78128c1f0496e5141743dc356a78f7c0083d
Mampel Lao <aamp@gft.com>
2020-02-06 16:58:50 +0100

    feat: added card component

--- projects/core/src/lib/card/card.component.ts
+++ projects/core/src/lib/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'lib-card',
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
+  @Input() name: string;
+  @Output() myEvent: EventEmitter<any> = new EventEmitter();
   constructor() { }
 
   ngOnInit() {
---
+++ src/stories/Core.stories.ts
@@ -0,0 +1,38 @@
+import { CoreComponent } from 'projects/core/src/public-api';
+import {storiesOf, moduleMetadata} from '@storybook/angular'
+import { CardComponent } from 'projects/core/src/lib/card/card.component';
+import { action } from '@storybook/addon-actions';
+import { CoreModule } from 'core/core';
+
+
+export default {
+  title: 'Core lib',
+  component: CoreComponent,
+};
+
+
+
+storiesOf('Core-Button', module)
+  .add('button enabled', () => ({
+    component: CoreComponent,
+    props: {
+      text: 'button from core',
+      disabled :false
+    }
+  }))
+  .add('button disabled', () => ({
+    component: CoreComponent,
+    props: {
+      text: 'button disabled',
+      disabled: true
+    }
+  }));
+  storiesOf('Core-Card', module)
+  
+  .add('button enabled', () => ({
+    component: CardComponent,
+    props: {
+      name: 'Mampel',
+      myEvent: action('Hello Mampel')
+    }
+  }))
